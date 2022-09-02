80b5fc89f5211d1c189230d0351133fbf9075e76
Prakash Yogi <yogipra2003@gmail.com>
2022-09-02 09:01:59 +0530

    Added static root to django setttings

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -145,3 +145,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = Path.joinpath(BASE_DIR, 'static'),
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'static')
